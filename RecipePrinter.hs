1dddc7d3f04c50471624f6b3c449019208beabd4
Unknown <burtannia1997@gmail.com>
2017-10-22 04:54:55 +0100

    Added functions to manipulate time

--- RecipePrinter.hs
+++ RecipePrinter.hs
@@ -9,7 +9,35 @@ import           Recipe
 --             | Sequence Recipe Recipe
 --             deriving (Show)
 
--- Recipe -> Int (change in time)
+-------------------------------------
+-- TIMING RECIPES
+-------------------------------------
+
+calcTime :: Recipe -> Int
+calcTime (Ingredient _)   = 0
+calcTime (Heat t r)       = 0 + calcTime r -- need something here
+calcTime (Combine r1 r2)  = calcTime r1 + calcTime r2
+calcTime (Wait t)         = t
+calcTime (Sequence r1 r2) = calcTime r1 + calcTime r2
+-- time to heat something depends on temp, volume and what it is
+
+atTime :: Int -> Recipe -> Recipe
+atTime t r = if t >= calcTime r
+                then r
+                else case r of
+                    (Heat t r') ->
+                        if t >= calcTime r'
+                            then r
+                            else atTime t r'
+                    (Combine r1 r2) -> -- bit awkward because combine gives no promise of order
+                        if t >= calcTime r1
+                            then atTime t r2
+                            else atTime t r1
+                    (Sequence r1 r2) ->
+                        if t >= calcTime r1
+                            then atTime t r2
+                            else atTime t r1
+                    _ -> r
 
 -------------------------------------
 -- PRINTING RECIPES
@@ -92,4 +120,5 @@ testList = [ ("milk", 1.00)
 
 -- Obviously this is rubbish... We need to take into account amortised costs
 -- type PricedItem = (String, Price, Quantity)
--- Use the measurement constructor on recipes
+-- Use the measurement constructor on recipes, at the moment we have a cooking show
+-- setup where we presume everything is already chopped and measured etc.
