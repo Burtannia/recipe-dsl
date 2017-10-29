a2b528e43d65563c7eb0943db000d85fbf285dc4
Unknown <burtannia1997@gmail.com>
2017-10-29 03:11:56 +0000

    Changed Recipe type

--- Recipe/Recipe.hs
+++ Recipe/Recipe.hs
@@ -10,7 +10,7 @@ teabag = Ingredient "teabag"
 water = Ingredient "water"
 
 cupOfTea :: Recipe
-cupOfTea = milk >< ((teabag >< heat (temp 100) water) >>> wait 5)
+cupOfTea = milk >< ((teabag >< (water `heatAt` 100)) >>> wait 5)
 
 oliveOil, tomato, onion, garlic, tinnedTomatoes, bayLeaf, thyme, basil, vegStock :: Recipe
 oliveOil = Ingredient "olive oil"
@@ -33,49 +33,60 @@ kidneyBeans = Ingredient "kidney beans"
 redChilli = Ingredient "red chilli"
 coriander = Ingredient "coriander"
 
+tomatoSauce :: Recipe
+tomatoSauce = (((((oliveOil `heatAt` 2) >< onion >< garlic) >>> wait 5)
+              >< tinnedTomatoes >< measure 6 tomato >< vegStock >< bayLeaf >< thyme)
+              >>> wait 20) >< basil
+
 chilliConCarne :: Recipe
-chilliConCarne = (((((((heat Medium oliveOil >< beefMince) >>> wait 5)
+chilliConCarne = ((((((((oliveOil `heatAt` 2) >< beefMince) >>> wait 5)
                     >< cumin >< paprika >< cayenne) >>> wait 5)
                     >< tomatoSauce >< beefStock) >>> wait 30)
                     >< kidneyBeans >< redChilli >< coriander) >>> wait 10
-
-tomatoSauce :: Recipe
-tomatoSauce = ((((heat Medium oliveOil >< onion >< garlic) >>> wait 5)
-                >< tinnedTomatoes >< tomato >< vegStock >< bayLeaf >< thyme)
-                >>> wait 20) >< basil -- need to remove bayLeaf
 
 -------------------------------------
 -- RECIPE DEFINITION
 -------------------------------------
 
-type Time = Int
-
-data Temperature = Degrees Int | High | Medium | Low
-    deriving Show
-
 data Recipe = Ingredient String
-            | Heat Temperature Recipe
+            | Heat
+            | Time
+            | Void
+            | Optional Recipe
+            | Measure Quantity Recipe
             | Combine Recipe Recipe
-            | Wait Time
             | Sequence Recipe Recipe
-            deriving (Show)
-            -- Inverse Recipe e.g. Heat -> Cool, Combine -> Remove
+            deriving Show
 
-temp :: Int -> Temperature
-temp = Degrees
+type Quantity = Int
 
-heat :: Temperature -> Recipe -> Recipe
-heat = Heat
+-- With adding the Measure combinator we can extract
+-- any quantifiable combinators into the measure combinator
+-- e.g. Wait is a quantity of time, heat int recipe is a quantity of heat
+
+optional :: Recipe -> Recipe
+optional = Optional
+
+measure :: Quantity -> Recipe -> Recipe
+measure = Measure
 
 (><) :: Recipe -> Recipe -> Recipe
 (><) = Combine
 
-wait :: Time -> Recipe
-wait = Wait
-
 -- r1 then r2
 (>>>) :: Recipe -> Recipe -> Recipe
 (>>>) = Sequence
+
+-- Can now build up some new combinators
+
+heatAt :: Recipe -> Quantity -> Recipe
+r `heatAt` t = (measure t Heat) >< r
+
+forDuration :: Recipe -> Quantity -> Recipe
+r `forDuration` q = (measure q Time) >< r
+
+wait :: Quantity -> Recipe
+wait q = (measure q Time) >< Void
 
 -------------------------------------
 -- UTILITY FUNCTIONS
@@ -87,47 +98,57 @@ extractSteps = extractStepsL . labelRecipe
 
 -- Create a list of steps of a LabelledRecipe
 extractStepsL :: LabelledRecipe -> [LabelledRecipe]
-extractStepsL (LIngredient _)      = []
-extractStepsL x@(LHeat _ _ r)      = extractStepsL r ++ [x]
+extractStepsL x@(LOptional _ r)    = extractStepsL r ++ [x]
+extractStepsL x@(LMeasure _ _ r)   = extractStepsL r ++ [x]
 extractStepsL x@(LCombine _ r1 r2) = extractStepsL r1 ++ extractStepsL r2 ++ [x]
 extractStepsL (LSequence r1 r2)    = extractStepsL r1 ++ extractStepsL r2
-extractStepsL r                    = [r]
-
--- Create a list of ingredients in a recipe
-getIngredients :: Recipe -> [String]
-getIngredients (Ingredient s)   = [s]
-getIngredients (Heat _ r)       = getIngredients r
-getIngredients (Combine r1 r2)  = getIngredients r1 ++ getIngredients r2
-getIngredients (Wait _)         = []
-getIngredients (Sequence r1 r2) = getIngredients r1 ++ getIngredients r2
+extractStepsL _                    = []
 
 -------------------------------------
 -- RECIPE LABELLING
 -------------------------------------
 
-type Label = Int
+-- r' is a LabelledRecipe therefore we can't construct
+-- Optional r'.
+
+-- labelRecipe' l (Optional r)     = (l', Optional r')
+-- where
+--   r' = labelRecipe' l r
+--   l' = calcLabel l r'
 
 data LabelledRecipe = LIngredient String
-                    | LHeat Label Temperature LabelledRecipe
+                    | LHeat
+                    | LTime
+                    | LVoid
+                    | LOptional Label LabelledRecipe
+                    | LMeasure Label Quantity LabelledRecipe
                     | LCombine Label LabelledRecipe LabelledRecipe
-                    | LWait Label Time
                     | LSequence LabelledRecipe LabelledRecipe
-                    deriving (Show)
+                    deriving Show
+
+type Label = Int
 
 getLabel :: LabelledRecipe -> Label
-getLabel (LIngredient _)  = 0
-getLabel (LHeat l _ _)    = l
+getLabel (LOptional l _)  = l
+getLabel (LMeasure l _ _) = l
 getLabel (LCombine l _ _) = l
-getLabel (LWait l _)      = l
-getLabel (LSequence _ _)  = 0
+getLabel _                = 0
 
 labelRecipe :: Recipe -> LabelledRecipe
 labelRecipe = labelRecipe' 1
 
 labelRecipe' :: Label -> Recipe -> LabelledRecipe
 labelRecipe' _ (Ingredient s)   = LIngredient s
+labelRecipe' _ Heat             = LHeat
+labelRecipe' _ Time             = LTime
+labelRecipe' _ Void             = LVoid
 
-labelRecipe' l (Heat t r)       = LHeat l' t r'
+labelRecipe' l (Optional r)     = LOptional l' r'
+                                  where
+                                    r' = labelRecipe' l r
+                                    l' = calcLabel l r'
+
+labelRecipe' l (Measure q r)    = LMeasure l' q r'
                                   where
                                     r' = labelRecipe' l r
                                     l' = calcLabel l r'
@@ -139,65 +160,41 @@ labelRecipe' l (Combine r1 r2)  = LCombine l'' lr1 lr2
                                     l'  = calcLabel l lr1
                                     l'' = calcLabel l' lr2
 
-labelRecipe' l (Wait t)         = LWait l t
-
 labelRecipe' l (Sequence r1 r2) = LSequence lr1 lr2
                                   where
                                     lr1 = labelRecipe' l r1
                                     lr2 = labelRecipe' l' r2
                                     l'  = calcLabel l lr1
-                                    l'' = calcLabel l' lr2
 
 calcLabel :: Label -> LabelledRecipe -> Label
 calcLabel l r' = case r' of
-    (LIngredient s)  -> l
+    (LIngredient _)  -> l
+    LHeat            -> l
+    LTime            -> l
+    LVoid            -> l
     (LSequence _ r2) -> getLabel r2 + 1
     _                -> getLabel r' + 1
 
 -------------------------------------
--- MORE COMPLEX DEFINITIONS
+-- RECIPE SEMANTICS
 -------------------------------------
+-- RECIPE:
+-- Ingredients
+-- time to make
+-- actions to make
 
--- data Quantity = Measurement Int | Fraction Int Int
---
--- instance Show Quantity where
---         show (Measurement i) = show i
---         show (Fraction n d) = "take " ++ show n ++ "//" ++ show d ++ " of the recipe"
---
--- data Position = Beside | Above | Wrap
---
--- instance Show Position where
--- 	show Beside = "and place it next to"
--- 	show Above = "and place it on top of"
--- 	show Wrap = "and wrap it in"
+-- Recipe is WHAT you do, Action is HOW you do it
+-- We describe what a recipe IS, we must describe the execution
+-- A recipe gets "fed" time and progresses in completion
+-- Recipe -> Time -> Recipe
+-- if Time > timeOf Recipe then Recipe is completed
 
--- data Recipe = Ingredient String
---             | Heat Temperature Recipe
---             | Combine Recipe Recipe
---             | Prepare Recipe -- cut, slice, peel etc.
---             | Wait Time
---             | Assemble Position Recipe Recipe
---             | After Recipe Recipe
---             | Measure Quantity Recipe
---             deriving (Show)
+data Action = Get Recipe
+            | Tag String Recipe
 
--- prepare :: Recipe -> Recipe
--- prepare = Prepare
---
--- measure :: Quantity -> Recipe -> Recipe
--- measure = Measure
---
--- -- assemble vertically (r1 above r2)
--- (^^^) :: Recipe -> Recipe -> Recipe
--- r1 ^^^ r2 = Assemble Above r1 r2
---
--- -- assemble horizontally
--- (~~~) :: Recipe -> Recipe -> Recipe
--- r1 ~~~ r2 = Assemble Beside r1 r2
---
--- -- wrap r2 in r1
--- (@@@) :: Recipe -> Recipe -> Recipe
--- r1 @@@ r2 = Assemble Wrap r1 r2
+--semantics :: Recipe -> (Time -> Action)
 
--- Must consider how to deal with splitting a recipe, at the moment it would be something like this:
--- (orangeZest >< (measure (Fraction 1 2) cakeMix)) ^^^ (lemonZest >< (measure (Fraction 1 2) cakeMix)
+-- Abstract semantics: set of actions that correspond directly to cooking techniques
+-- these actions are "performed" when given Time
+
+-- Concrete implementation: various simulation models e.g. professional with brigade
