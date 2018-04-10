2743c710e6a58e8da73557ca2108881ef2591c60
James <burtannia1997@gmail.com>
2018-04-11 00:57:58 +0100

    QuickSpec stuff

--- Recipe/QS.hs
+++ Recipe/QS.hs
@@ -10,18 +10,29 @@ import Test.QuickCheck
 import Control.Monad (liftM, liftM2, liftM3)
 import Data.Tree
 import Recipe.Kitchen
+import Data.List (sort)
 
 -------------------------------------
 -- Arbitrary Instances
 -------------------------------------
 
+-- quickCheck (\s r1 r2 -> combine s r1 r2 == combine s r2 r1)
+
 instance {-# OVERLAPPING #-} Arbitrary Recipe where
-    arbitrary = sized $ \n ->
-        let bin = resize (n `div` 2) arbitrary
-            un  = resize (n-1) arbitrary
-        in case n of
-            1 -> genIng
-            _ -> oneof [genUnRec un, genBinRec bin bin]
+    arbitrary = sized $ \n -> do
+        if n <= 5 then
+            genRecipe n
+        else
+            resize 5 arbitrary
+        
+genRecipe :: Int -> Gen Recipe
+genRecipe n =
+    let un = resize (n-1) arbitrary
+        bin = resize (n `div` 2) arbitrary
+     in if n == 0 then
+            genIng
+        else
+            oneof [genUnRec un, genBinRec bin bin]
 
 genIng :: Gen Recipe
 genIng = liftM (ingredient . show) (choose (1, 100) :: Gen Int)
@@ -81,6 +92,22 @@ instance Observe [Obs] Bool Condition where
 -- QuickSpec Stuff
 -------------------------------------
 
+-- newtype Recipe' = Recipe' Recipe
+--     deriving Show
+
+-- instance Ord Recipe' where
+--     compare (Recipe' r1) (Recipe' r2) =
+--         let xs = sort $ topologicals r1
+--             ys = sort $ topologicals r2
+--          in compare xs ys
+
+-- instance Eq Recipe' where
+--     (==) r1 r2 =
+--         compare r1 r2 == EQ 
+
+-- instance Arbitrary Recipe' where
+--     arbitrary = liftM Recipe' arbitrary
+
 qsRecipe = quickSpec
     [ withMaxTermSize 5
     
@@ -95,13 +122,25 @@ qsRecipe = quickSpec
     , con "transaction" (transaction :: Recipe -> Recipe)
     , con "measure" (measure :: Measurement -> Recipe -> Recipe)
 
-    -- , con "optional" (optional :: String -> Recipe -> Recipe)
-    -- , con "toTemp" (toTemp :: Int -> Recipe -> Recipe)
-    -- , con "forTime" (forTime :: Time -> Recipe -> Recipe)
-    -- , con "hours" (hours :: Int -> Time)
-    -- , con "minutes" (minutes :: Int -> Time)
+    -- , con "ingredient" (\s -> Recipe' (ingredient s))
+    -- , con "heat" (\r -> Recipe' (heat r))
+    -- , con "heatAt" (\t r -> Recipe' (heatAt t r))
+    -- , con "wait" (\r -> Recipe' (wait r))
+    -- , con "combine" (\s r1 r2 -> Recipe' (combine s r1 r2))
+    -- , con "addCondition" (\c r -> Recipe' (addCondition c r))
+    -- , con ".&&" ((.&&) :: Condition -> Condition -> Condition)
+    -- , con ".||" ((.||) :: Condition -> Condition -> Condition)
+    -- , con "transaction" (\r -> Recipe' (transaction r))
+    -- , con "measure" (\m r -> Recipe' (measure m r))
+
+    , con "optional" (optional :: String -> Recipe -> Recipe)
+    , con "toTemp" (toTemp :: Int -> Recipe -> Recipe)
+    , con "forTime" (forTime :: Time -> Recipe -> Recipe)
+    , con "hours" (hours :: Time -> Time)
+    , con "minutes" (minutes :: Time -> Time)
 
     , monoType (Proxy :: Proxy Recipe)
+    -- , monoType (Proxy :: Proxy Recipe')
     , monoType (Proxy :: Proxy Measurement)
-    --, monoTypeObserve (Proxy :: Proxy Condition)
+    , monoTypeObserve (Proxy :: Proxy Condition)
     , monoType (Proxy :: Proxy Time) ]
